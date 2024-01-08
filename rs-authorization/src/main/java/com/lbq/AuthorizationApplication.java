389b3dbbab1e3d69d055bc4a362987ffdd90c45e
刘斌权 <1134914623@qq.com>
2024-01-08 10:28:34 +0800

    初始化

---
+++ rs-authorization/src/main/java/com/lbq/AuthorizationApplication.java
@@ -0,0 +1,16 @@
+package com.lbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lbq
+ * @Date: 2024/1/8
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class AuthorizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class, args);
+    }
+}
---
+++ rs-gateway/src/main/java/com/lbq/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.lbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lbq
+ * @Date: 2024/1/8
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
