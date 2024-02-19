ac9dcf8c07d18af13bc21ca85769c75529b7e220
刘斌权 <1134914623@qq.com>
2024-02-19 16:37:06 +0800

    调度服务

---
+++ rs-modules/rs-job/src/main/java/com/lbq/JobApplication.java
@@ -0,0 +1,18 @@
+package com.lbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 调度服务
+ *
+ * @Author: lbq
+ * @Date: 2024/2/19
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class JobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+}
