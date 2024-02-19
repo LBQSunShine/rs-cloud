5faf7a878c4279570a1c8fec99475be542a55469
刘斌权 <1134914623@qq.com>
2024-02-19 11:29:27 +0800

    文件上传模块

---
+++ rs-modules/rs-file/src/main/java/com/lbq/FileApplication.java
@@ -0,0 +1,16 @@
+package com.lbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lbq
+ * @Date: 2024/2/19
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
