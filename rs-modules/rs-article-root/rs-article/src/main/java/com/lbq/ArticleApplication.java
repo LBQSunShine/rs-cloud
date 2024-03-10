c2bfbbe306190580287a3c34413320999b49777b
LBQSunShine <1134914623@qq.com>
2024-03-10 14:30:35 +0800

    文章模块

---
+++ rs-modules/rs-article-root/rs-article/src/main/java/com/lbq/ArticleApplication.java
@@ -0,0 +1,16 @@
+package com.lbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lbq
+ * @Date 2024/3/10
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
